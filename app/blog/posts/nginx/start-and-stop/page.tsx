9cb4ce0acb8a94e06d122cfbb309c4e7d55a6b70
luoyangwei <luoyangwei@rsjia.com>
2025-06-23 19:39:26 +0800

    first commit

---
+++ app/blog/posts/nginx/start-and-stop/page.tsx
@@ -0,0 +1,5 @@
+import NginxStartAndStop from "@/content/nginx-start-and-stop.mdx"
+
+export default function NginxStartAndStopPage() {
+    return <NginxStartAndStop/>
+}
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
